ce5ca9569a345c8d1b8bc9749a92e5298332a402
Débora Lee <debora.leesilva@gmail.com>
2022-08-06 17:03:22 -0300

    Sistema de contagem de peças, encontro remoto 1

---
+++ sistema-contagem-peças.js
@@ -0,0 +1,38 @@
+//se a peça possuir um peso superior a 100 gramas, pode cadastrar
+//dada a capacidade de cada caixa, caso a lista de peças seja superior a 10, imprima uma mensagem informando não ter capacidade suficiente
+//caso a peça tenha um nome com quantidade inferior a 3 caracteres, informe uma mensagem de erro
+
+let capacidadeLimiteCaixa=listaPeças
+
+
+let pesoPeça= parseFloat window.prompt("Digite o peso da peça:")
+
+if (pesoPeça>100) {
+    pesoPeça.push(listaPeças);
+
+    if (listaPeças.lenght>10) {
+        console.log ("Limite de capacidade excedido")
+
+        if (nomePeça.lenght<3) {
+            alert ("Nome inválido")
+
+
+        } else {
+            console.log ("Peça cadastrada com sucesso!")
+
+            
+        }
+        
+    } else {
+        nomePeça= parseFloat window.prompt("Digite o nome da peça:")
+        
+    }
+    
+} else {
+    console.log ("Peso minímo não atingido")
+    
+}
+
+
+    
+    
